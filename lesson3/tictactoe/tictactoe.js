d6477c796514d7f826f7b51083b7db5c92777593
Jacquie Des Rosiers <jacquiedesrosiers@gmail.com>
2023-04-26 19:48:02 -0400

    fixed (almost) all eslint errors

--- lesson3/tictactoe/tictactoe.js
+++ lesson3/tictactoe/tictactoe.js
@@ -3,22 +3,23 @@ const readline = require('readline-sync');
 const INITIAL_MARKER = ' ';
 const HUMAN_MARKER = 'X';
 const COMPUTER_MARKER = 'O';
-const COMPUTER_2_MARKER = '#';
+const COMP_2_MARKER = '#';
 const NUMBER_OF_SQUARES = 25;
 const MATCH_ROUND_COUNT = 3;
 const WINNING_LINES = [
   // Rows
-  [1, 2, 3], [2, 3, 4], [3, 4, 5], [ 6, 7, 8], [7, 8, 9], [8, 9, 10], [10, 11, 12],
-  [12, 13, 14], [13, 14, 15], [16, 17, 18], [17, 18, 19], [18, 19, 20], [21, 22, 23],
-  [22, 23, 24], [23, 24, 25],
+  [1, 2, 3], [2, 3, 4], [3, 4, 5], [ 6, 7, 8], [7, 8, 9], [8, 9, 10],
+  [10, 11, 12], [12, 13, 14], [13, 14, 15], [16, 17, 18], [17, 18, 19],
+  [18, 19, 20], [21, 22, 23], [22, 23, 24], [23, 24, 25],
   // Columns
-  [1, 6, 11], [6, 11, 16], [11, 16, 21], [2, 7, 12], [7, 12, 17], [12, 17, 22], [3, 8, 13],
-  [8, 13, 18], [13, 18, 23], [4, 9, 14], [9, 14, 19], [14, 19, 24], [5, 10, 15], [10, 15, 20],
-  [15, 20, 25],
+  [1, 6, 11], [6, 11, 16], [11, 16, 21], [2, 7, 12], [7, 12, 17], [12, 17, 22],
+  [3, 8, 13], [8, 13, 18], [13, 18, 23], [4, 9, 14], [9, 14, 19], [14, 19, 24],
+  [5, 10, 15], [10, 15, 20], [15, 20, 25],
   // Diagonal
-  [1, 7, 13], [2, 8, 14], [3, 7, 11], [3, 9, 15], [4, 8, 12], [5, 9, 13], [6, 12, 18], [7, 13, 19],
-  [8, 12, 16], [8, 14, 20], [9, 13, 17], [10, 14, 18], [11, 17, 23], [12, 18, 24], [13, 17, 21],
-  [13, 19, 25], [14, 18, 22], [15, 19, 23],
+  [1, 7, 13], [2, 8, 14], [3, 7, 11], [3, 9, 15], [4, 8, 12], [5, 9, 13],
+  [6, 12, 18], [7, 13, 19], [8, 12, 16], [8, 14, 20], [9, 13, 17], [10, 14, 18],
+  [11, 17, 23], [12, 18, 24], [13, 17, 21], [13, 19, 25], [14, 18, 22],
+  [15, 19, 23],
 ];
 const CORNERS = [1, 5, 21, 25];
 const CENTER_OF_BOARD = 13;
@@ -27,7 +28,7 @@ const PLAYERS = ['player', 'computer 1', 'computer 2'];
 function displayBoard(board) {
   console.clear();
 
-  console.log(`You are ${HUMAN_MARKER}. Computer 1 is ${COMPUTER_MARKER}. Computer 2 is ${COMPUTER_2_MARKER}.`);
+  console.log(`You are ${HUMAN_MARKER}. Computer 1 is ${COMPUTER_MARKER}. Computer 2 is ${COMP_2_MARKER}.`);
 
   console.log('');
   console.log('     |     |     |     |');
@@ -72,7 +73,7 @@ function boardFull(board) {
   return emptySquares(board).length === 0;
 }
 
-function displayCurrentScore(scores, scoreboardTitle) {
+function displayScore(scores, scoreboardTitle) {
   prompt(`${scoreboardTitle}`);
   for (const player in scores) {
     prompt(`${player} - ${scores[player]}`);
@@ -81,17 +82,21 @@ function displayCurrentScore(scores, scoreboardTitle) {
 }
 
 function displayOverallWinner(scores) {
-  if ((scores['Player'] > scores['Computer 1']) && (scores['Player'] > scores['Computer 2'])) {
+  let playerScore = scores['Player'];
+  let comp1Score = scores['Computer 1'];
+  let comp2Score = scores['Computer 2'];
+
+  if ((playerScore > comp1Score) && (playerScore > comp2Score)) {
     prompt('Player won the match! Congratulations.');
-  } else if ((scores['Computer 1'] > scores['Computer 2']) && (scores['Computer 1'] > scores['Player'])) {
+  } else if ((comp1Score > comp2Score) && (comp1Score > playerScore)) {
     prompt('Computer 1 won the match! Good game.');
-  } else if ((scores['Computer 2'] > scores['Computer 1']) && (scores['Computer 2'] > scores['Player'])) {
+  } else if ((comp2Score > comp1Score) && (comp2Score > playerScore)) {
     prompt('Computer 2 won the match! Better luck next time, Player and Computer 1.');
-  } else if ((scores['Player'] === scores['Computer 1']) && (scores['Player'] > scores['Computer 2'])) {
+  } else if ((playerScore === comp1Score) && (playerScore > comp2Score)) {
     prompt('Player and Computer 1 reigned champions! Great game.');
-  } else if ((scores['Player'] === scores['Computer 2']) && (scores['Player'] > scores['Computer 1'])) {
+  } else if ((playerScore === comp2Score) && (playerScore > comp1Score)) {
     prompt('Player and Computer 2 are the winner! Great game.');
-  } else if ((scores['Computer 1'] === scores['Computer 2']) && (scores['Computer 1'] > scores['Player'])) {
+  } else if ((comp1Score === comp2Score) && (comp1Score > playerScore)) {
     prompt('Player and Computer 1 reigned champions! Great game.');
   } else {
     prompt('The match was a tie! Three strong contenders indeed.');
@@ -102,24 +107,15 @@ function detectWinner(board) {
   for (let line = 0; line < WINNING_LINES.length; line += 1) {
     const [sq1, sq2, sq3] = WINNING_LINES[line];
 
-    if (
-      board[sq1] === HUMAN_MARKER
-      && board[sq2] === HUMAN_MARKER
-      && board[sq3] === HUMAN_MARKER
-    ) {
+    if ( board[sq1] === HUMAN_MARKER && board[sq2] === HUMAN_MARKER
+      && board[sq3] === HUMAN_MARKER) {
       return 'Player';
     } else if (
-      board[sq1] === COMPUTER_MARKER
-      && board[sq2] === COMPUTER_MARKER
-      && board[sq3] === COMPUTER_MARKER
-
-    ) {
+      board[sq1] === COMPUTER_MARKER && board[sq2] === COMPUTER_MARKER
+      && board[sq3] === COMPUTER_MARKER) {
       return 'Computer 1';
-    } else if (
-      board[sq1] === COMPUTER_2_MARKER
-      && board[sq2] === COMPUTER_2_MARKER
-      && board[sq3] === COMPUTER_2_MARKER
-    ) {
+    } else if ( board[sq1] === COMP_2_MARKER
+      && board[sq2] === COMP_2_MARKER && board[sq3] === COMP_2_MARKER) {
       return 'Computer 2';
     }
   }
@@ -133,11 +129,15 @@ function someoneWon(board) {
 
 function detectMajorityGamesWon(scores) {
   const majority = Math.ceil(MATCH_ROUND_COUNT / 2);
-  return (scores['Player'] === majority) || (scores['Computer 1'] === majority) || (scores['Computer 2'] === majority);
+  return (scores['Player'] === majority) || (scores['Computer 1'] === majority)
+    || (scores['Computer 2'] === majority);
 }
 
 function incrementWinnerPoints(scores, winner) {
-  return scores[winner] += 1;
+  if (winner) {
+    scores[winner] += 1;
+  }
+  return undefined;
 }
 
 function joinOr(arr, delimiter = ', ', word = 'or') {
@@ -154,7 +154,7 @@ function joinOr(arr, delimiter = ', ', word = 'or') {
   }
 }
 
-function playerChoosesSquare(board, scores) {
+function playerChoosesSquare(board) {
   if (someoneWon(board)) {
     return 0;
   }
@@ -171,65 +171,85 @@ function playerChoosesSquare(board, scores) {
   }
 
   board[square] = HUMAN_MARKER;
-  displayBoard(board);
-  displayCurrentScore(scores, 'CURRENT SCOREBOARD');
   return undefined;
 }
 
-function computerChoosesSquare(board, scores, currentPlayerMarker, opposition1, opposition2) {
-  if (someoneWon(board)) {
-    return 0;
-  }
-
+function computerOffense(board, rival1, rival2) {
   let square;
-
-  // Offense first
   for (let index = 0; index < WINNING_LINES.length; index += 1) {
     const line = WINNING_LINES[index];
-    square = findAtRiskSquare(line, board, opposition1, opposition2);
+    square = findAtRiskSquare(line, board, rival1, rival2);
     if (square) break;
   }
 
-  // Defense
-  if (!square) {
-    for (let index = 0; index < WINNING_LINES.length; index += 1) {
-      const line = WINNING_LINES[index];
-      square = findAtRiskSquare(line, board, opposition1, opposition2);
-      if (square) break;
-    }
+  return square;
+}
+
+function computerDefense(board, rival1, rival2) {
+  let square;
+  for (let index = 0; index < WINNING_LINES.length; index += 1) {
+    const line = WINNING_LINES[index];
+    square = findAtRiskSquare(line, board, rival1, rival2);
+    if (square) break;
   }
 
-  // Pick center square on board if available
-  if (!square) {
-    if (board[CENTER_OF_BOARD] === INITIAL_MARKER) {
-      square = CENTER_OF_BOARD;
+  return square;
+}
+
+function checkCenterOfBoardAvail(board) {
+  let square;
+  if (board[CENTER_OF_BOARD] === INITIAL_MARKER) {
+    square = CENTER_OF_BOARD;
+  } else {
+    square = null;
+  }
+  return square;
+}
+
+function checkBoardCornersAvail(board) {
+  let square;
+  for (let index = 0; index < CORNERS.length; index += 1) {
+    const line = CORNERS[index];
+    if (board[line] === INITIAL_MARKER) {
+      square = CORNERS[index];
+      break;
     } else {
       square = null;
     }
   }
+  return square;
+}
 
-  // Pick one of the corner squares of board if available
-  if (!square) {
-    for (let index = 0; index < CORNERS.length; index += 1) {
-      const line = CORNERS[index];
-      if (board[line] === INITIAL_MARKER) {
-        square = CORNERS[index];
-        break;
-      } else {
-        square = null;
-      }
-    }
+function getSquareAtRandomIndex(board) {
+  const randomIndex = Math.floor(Math.random() * emptySquares(board).length);
+  let square = emptySquares(board)[randomIndex];
+  return square;
+}
+
+function computerChoosesSquare(board, marker, rival1, rival2) {
+  if (someoneWon(board)) {
+    return 0;
   }
 
-  // Pick random square
+  let square = computerOffense(board, rival1, rival2);
+
   if (!square) {
-    const randomIndex = Math.floor(Math.random() * emptySquares(board).length);
-    square = emptySquares(board)[randomIndex];
+    square = computerDefense(board, rival1, rival2);
   }
 
-  board[square] = currentPlayerMarker;
-  displayBoard(board);
-  displayCurrentScore(scores, 'CURRENT SCOREBOARD');
+  if (!square) {
+    square = checkCenterOfBoardAvail(board);
+  }
+
+  if (!square) {
+    square = checkBoardCornersAvail(board);
+  }
+
+  if (!square) {
+    square = getSquareAtRandomIndex(board);
+  }
+
+  board[square] = marker;
   return undefined;
 }
 
@@ -237,14 +257,14 @@ function findAtRiskSquare(line, board, marker1, marker2) {
   const markersInLine = line.map((square) => board[square]);
 
   if (markersInLine.filter((val) => val === marker1).length === 2) {
-    const unusedSquare = line.find((square) => board[square] === INITIAL_MARKER);
-    if (unusedSquare !== undefined) {
-      return unusedSquare;
+    const emptySquare = line.find((square) => board[square] === INITIAL_MARKER);
+    if (emptySquare !== undefined) {
+      return emptySquare;
     }
   } else if (markersInLine.filter((val) => val === marker2).length === 2) {
-    const unusedSquare = line.find((square) => board[square] === INITIAL_MARKER);
-    if (unusedSquare !== undefined) {
-      return unusedSquare;
+    const emptySquare = line.find((square) => board[square] === INITIAL_MARKER);
+    if (emptySquare !== undefined) {
+      return emptySquare;
     }
   }
   return null;
@@ -254,9 +274,11 @@ function chooseSquare(board, currentPlayer, scores) {
   if (currentPlayer === 'player') {
     return playerChoosesSquare(board, scores);
   } else if (currentPlayer === 'computer 1') {
-    return computerChoosesSquare(board, scores, COMPUTER_MARKER, HUMAN_MARKER, COMPUTER_2_MARKER);
+    return computerChoosesSquare(board, COMPUTER_MARKER,
+      HUMAN_MARKER, COMP_2_MARKER);
   } else if (currentPlayer === 'computer 2') {
-    return computerChoosesSquare(board, scores, COMPUTER_2_MARKER, HUMAN_MARKER, COMPUTER_MARKER);
+    return computerChoosesSquare(board, COMP_2_MARKER,
+      HUMAN_MARKER, COMPUTER_MARKER);
   }
   return undefined;
 }
@@ -325,15 +347,13 @@ while (true) {
       displayBoard(board);
       prompt(`Current round: ${roundCount} / 3`);
       console.log(''); // print empty line
-      displayCurrentScore(scores, 'CURRENT SCOREBOARD');
+      displayScore(scores, 'CURRENT SCOREBOARD');
 
       chooseSquare(board, firstMovePlayer, scores);
       firstMovePlayer = alternatePlayer(firstMovePlayer);
       if (someoneWon(board) || boardFull(board)) {
         const winner = detectWinner(board);
-        if (winner) {
-          incrementWinnerPoints(scores, winner);
-        }
+        incrementWinnerPoints(scores, winner);
         resetBoard(board);
         break;
       }
@@ -350,7 +370,7 @@ while (true) {
   prompt('And that\'s the game!');
   console.log(''); // print empty line
 
-  displayCurrentScore(scores, 'FINAL SCOREBOARD');
+  displayScore(scores, 'FINAL SCOREBOARD');
   displayOverallWinner(scores);
   console.log(''); // print empty line
 
